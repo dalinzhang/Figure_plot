0348ff8a7b31b874be0e8537146851e02c2b005d
dadafly133 <zhangdalin90@gmail.com>
2018-06-16 23:47:27 +1000

    remove comments

--- bar_chart.m
+++ bar_chart.m
@@ -1,44 +1,44 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % plot multiple bar chart
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% clear all;
-% 
-% file_name = "/home/dadafly/Doc/draft/CIKM_2018/auc_sub.xlsx";
-% [data, header] = xlsread(file_name);
-% 
-% train_proportion = data(:,1);
-% test_accuracy	 = data(:,2);
-% max_v			 = data(:,3);
-% min_v			 = data(:,4);
-% 
-% figure;
-% hold on;
-% 
-% h = bar(categorical(train_proportion), [min_v, test_accuracy, max_v], 'EdgeAlpha',0, 'BarWidth', 1); % EdgeAlpha sets the box line of the bar; BarWidth sets the width of the bar
-% ylim([0 1])
-% 
-% % color of the bar chart
-% colormap(summer(9)); 
-% 
-% % set the legend
-% l = cell(1,3);
-% l{1}='Min'; l{2}='Average'; l{3}='Max';   
-% lgd = legend(h,l, 'Box', 'on');
-% lgd.FontSize=35;
-% 
-% ylabel('Area Under ROC Curve','FontSize',50,'FontWeight','bold');
-% xlabel('Number of Training Subjects ','FontSize',30,'FontWeight','bold');
-% set(gca,'FontSize',40,'FontWeight','bold','linewidth',2);
-% 
-% box on
-% grid on
+clear all;
+
+file_name = "./data/auc_sub.xlsx";
+[data, header] = xlsread(file_name);
+
+train_proportion = data(:,1);
+test_accuracy	 = data(:,2);
+max_v			 = data(:,3);
+min_v			 = data(:,4);
+
+figure;
+hold on;
+
+h = bar(categorical(train_proportion), [min_v, test_accuracy, max_v], 'EdgeAlpha',0, 'BarWidth', 1); % EdgeAlpha sets the box line of the bar; BarWidth sets the width of the bar
+ylim([0 1])
+
+% color of the bar chart
+colormap(summer(9)); 
+
+% set the legend
+l = cell(1,3);
+l{1}='Min'; l{2}='Average'; l{3}='Max';   
+lgd = legend(h,l, 'Box', 'on');
+lgd.FontSize=35;
+
+ylabel('Area Under ROC Curve','FontSize',50,'FontWeight','bold');
+xlabel('Number of Training Subjects ','FontSize',30,'FontWeight','bold');
+set(gca,'FontSize',40,'FontWeight','bold','linewidth',2);
+
+box on
+grid on
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % plot bar chart with errorbar
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all;
 
-file_name = "/home/dadafly/Doc/draft/CIKM_2018/auc_sub.xlsx";
+file_name = "./data/auc_sub.xlsx";
 [data, header] = xlsread(file_name);
 
 train_proportion = data(:,1);
@@ -66,3 +66,5 @@ set(gca,'FontSize',40,'FontWeight','bold','linewidth',2);
 
 box on
 grid on
+
+
